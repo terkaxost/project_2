2b302ce36c2dcb1196c7657cd1a3d8fca86764b8
Nikolay Ermolaev <terkaxost@gmail.com>
2020-03-14 07:22:25 +0300

    Сборка от группового созвона 20.02.2020

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    devServer: {
+        open: true
+    },
+    mode: process.env.NODE_ENV,
+    devtool: 'eval-source-map',
+    module: {
+        rules: [
+            { test: /\.hbs$/, loader: "handlebars-loader" },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img'
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    process.env.NODE_ENV !== 'production'
+                        ? 'style-loader'
+                        : MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/views/index.hbs',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'main.css'
+        })
+    ]
+};
